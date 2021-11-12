593e9cb3382ad52a02eec9cfbe55db3f90179459
jase231 <91901364+jase231@users.noreply.github.com>
2021-11-11 20:05:57 -0500

    Add files via upload

---
+++ windows.bat
@@ -0,0 +1,159 @@
+@echo off
+net sessions
+if %errorlevel%==0 (
+echo Admin rights present
+) else (
+echo This script requires admin rights, exiting...
+pause
+exit
+)
+
+echo Enabling User Access Prompt ...
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+echo Do you want to disable the Remote Desktop Protocol? (DOUBLE CHECK THE README!!!)
+choice /c ync
+if ERRORLEVEL 1 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f && reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f && netsh advfirewall firewall set service type = remotedesktop mode = disable
+if ERRORLEVEL 2 REM continue
+echo Enabling Windows Automatic Updates ...
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 3 /f
+echo Flushing DNS cache ...
+ipconfig /flushdns
+net start | findstr Remote Registry
+if %errorlevel%==0 (
+	echo Attempting to stop Remote Registry
+	net stop RemoteRegistry
+	sc config RemoteRegistry start=disabled
+	if %errorlevel%==1 echo Stop failed... sorry...
+) else ( 
+	echo Remote Registry is not running, continuing ...
+)
+echo Deleting saved creds ...
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\*.*" /s /f /q
+del "%TEMP%\tokensonly.txt" /s /f /q
+echo net stopping remote access, telephony, fax, etc
+set SRVC_LIST=(RemoteAccess Telephony tlntsvr p2pimsvc simptcp fax msftpsvc)
+	for %%i in %HITHERE% do net stop %%i
+	for %%i in %HITHERE% sc config %%i start= disabled
+echo Disabling Remote Assistance, Telnet, netcat, DISM features, do not track reg keys
+netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Telnet Server" new enable=no >NUL
+netsh advfirewall firewall set rule name="netcat" new enable=no >NUL
+dism /online /disable-feature /featurename:IIS-WebServerRole >NUL
+dism /online /disable-feature /featurename:IIS-WebServer >NUL
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures >NUL
+dism /online /disable-feature /featurename:IIS-HttpErrors >NUL
+dism /online /disable-feature /featurename:IIS-HttpRedirect >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45 >NUL
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics >NUL
+dism /online /disable-feature /featurename:IIS-HttpLogging >NUL
+dism /online /disable-feature /featurename:IIS-LoggingLibraries >NUL
+dism /online /disable-feature /featurename:IIS-RequestMonitor >NUL
+dism /online /disable-feature /featurename:IIS-HttpTracing >NUL
+dism /online /disable-feature /featurename:IIS-Security >NUL
+dism /online /disable-feature /featurename:IIS-URLAuthorization >NUL
+dism /online /disable-feature /featurename:IIS-RequestFiltering >NUL
+dism /online /disable-feature /featurename:IIS-IPSecurity >NUL
+dism /online /disable-feature /featurename:IIS-Performance >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic >NUL
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools >NUL
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools >NUL
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility >NUL
+dism /online /disable-feature /featurename:IIS-Metabase >NUL
+dism /online /disable-feature /featurename:IIS-HostableWebCore >NUL
+dism /online /disable-feature /featurename:IIS-StaticContent >NUL
+dism /online /disable-feature /featurename:IIS-DefaultDocument >NUL
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing >NUL
+dism /online /disable-feature /featurename:IIS-WebDAV >NUL
+dism /online /disable-feature /featurename:IIS-WebSockets >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationInit >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET45 >NUL
+dism /online /disable-feature /featurename:IIS-ASP >NUL
+dism /online /disable-feature /featurename:IIS-CGI >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIFilter >NUL
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes >NUL
+dism /online /disable-feature /featurename:IIS-CustomLogging >NUL
+dism /online /disable-feature /featurename:IIS-BasicAuthentication >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic >NUL
+dism /online /disable-feature /featurename:IIS-ManagementConsole >NUL
+dism /online /disable-feature /featurename:IIS-ManagementService >NUL
+dism /online /disable-feature /featurename:IIS-WMICompatibility >NUL
+dism /online /disable-feature /featurename:IIS-LegacyScripts >NUL
+dism /online /disable-feature /featurename:IIS-LegacySnapIn >NUL
+dism /online /disable-feature /featurename:IIS-FTPServer >NUL
+dism /online /disable-feature /featurename:IIS-FTPSvc >NUL
+dism /online /disable-feature /featurename:IIS-FTPExtensibility >NUL
+dism /online /disable-feature /featurename:TFTP >NUL
+dism /online /disable-feature /featurename:TelnetClient >NUL
+dism /online /disable-feature /featurename:TelnetServer >NUL
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main" /v DoNotTrack /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Download" /v RunInvalidSignatures /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN\Settings" /v LOCALMACHINE_CD_UNLOCK /t REG_DWORD /d 1 /t
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonBadCertRecving /t REG_DWORD /d /1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnOnPostRedirect /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v WarnonZoneCrossing /t REG_DWORD /d 1 /f
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v DisablePasswordCaching /t REG_DWORD /d 1 /f 
+reg ADD HKCU\SYSTEM\CurrentControlSet\Services\CDROM /v AutoRun /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\CrashControl /v CrashDumpEnabled /t REG_DWORD /d 0 /f
+
+echo Changing registry policies ...
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeText /t REG_SZ /d "This is a secure device"
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v LegalNoticeCaption /t REG_SZ /d "Secured Device"
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
+echo Setting password policy ...
+net accounts /minpwlen:10
+net accounts /maxpwage:30
+net accounts /minpwage:5
+echo Setting auditing rules...
+auditpol /set /category:* /success:enable
+auditpol /set /category:* /failure:enable
+echo Modifying power settings ...
+powercfg -SETDCVALUEINDEX SCHEME_BALANCED SUB_NONE CONSOLELOCK 1
+powercfg -SETDCVALUEINDEX SCHEME_MIN SUB_NONE CONSOLELOCK 1
+powercfg -SETDCVALUEINDEX SCHEME_MAX SUB_NONE CONSOLELOCK 1
+
+echo Saving network shares to 'shares.txt' (make sure to check file later)
+net share > shares.txt
